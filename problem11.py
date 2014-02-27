15dc9d916d44c4490d34dcaaab629248fc998171
unknown <folson@TLTC-105W3.rd.unr.edu>
2014-02-27 15:10:53 -0800

    In this commit I added the text file containing the array to be analyzed and I figured out how to store the values from said matrix into a 2-dimentional array

--- problem11.py
+++ problem11.py
@@ -5,7 +5,21 @@
 
 def filter_matrix( file_name ):
     fout = open(file_name, 'r')
+    file_list = []
+
+    ### Store the matix from the text file in a 2-dimentional array
+    while (fout.readline() != ''):
+        file_line = fout.readline()
+        file_list.append(file_line.split())
+    
+    a, b, c = 0, 0, 0
+    ### Iterate through row groups and test
+    ### Iterate through column groups and check
+    ### Iterate through slant right groups and check
+    ### Iterate through slant left groups and check
+    print file_list[:]
     fout.close()
+    return a*b*c
 
 user_file = raw_input("Enter the file name of the matix: ")
 print filter_matrix(user_file)
