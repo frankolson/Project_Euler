737a08d07b3f733d40acc9f85d87dac66041c083
Frank Olson <will.olson11@gmail.com>
2014-10-02 15:33:30 -0700

    fixed problem 11

--- problem11.py
+++ problem11.py
@@ -2,26 +2,61 @@
 ### Will Olson
 ### 27 February 2014
 ### Python 2.7.6
+### Last edit: 09 September 2014
 
 def filter_matrix( file_name ):
-    fout = open(file_name, 'r')
+    fout = open(file_name, 'rU')
     file_list = []
 
+    # Answer variable
+    maxP = 0
+
     ### Store the matix from the text file in a 2-dimentional array
-    while (fout.readline() != ''):
-        file_line = fout.readline()
-        file_list.append(file_line.split())
+    for line in fout:
+        file_list.append(line.strip().split(' '))
+
+    file_list = [[int(j) for j in i] for i in file_list]
     
-    a, b, c = 0, 0, 0
-    ### Iterate through row groups and test
-    ### Iterate through column groups and check
-    ### Iterate through slant right groups and check
-    ### Iterate through slant left groups and check
     print file_list[:]
     fout.close()
-    return a*b*c
+
+    # Check horizontily
+    for i in range(len(file_list)):
+        for j in range(len(file_list[i])-3):
+            h = (file_list[i][j])*(file_list[i][j+1])*(file_list[i][j+2])*\
+                (file_list[i][j+3])
+
+            if h > maxP:
+                maxP = h
+    
+    # Check vertically
+    for i in range(len(file_list[i])-3):
+        for j in range(len(file_list)):
+            v = (file_list[i][j])*(file_list[i+1][j])*(file_list[i+2][j])*\
+                (file_list[i+3][j])
+
+            if v > maxP:
+                maxP = v
+
+    # Check left diagonal
+    for i in range(3, len(file_list[i])):
+        for j in range(len(file_list)-3):
+            leftD = (file_list[i][j])*(file_list[i-1][j+1])*(file_list[i-2][j+2])*\
+                (file_list[i-3][j+3])
+
+            if leftD > maxP:
+                maxP = leftD
+
+    # Check right diagonal
+    for i in range(len(file_list[i])-3):
+        for j in range(len(file_list)-3):
+            rightD = (file_list[i][j])*(file_list[i+1][j+1])*(file_list[i+2][j+2])*\
+                (file_list[i+3][j+3])
+
+            if rightD > maxP:
+                maxP = rightD
+    
+    return maxP
 
 user_file = raw_input("Enter the file name of the matix: ")
 print filter_matrix(user_file)
-
-### In progress
